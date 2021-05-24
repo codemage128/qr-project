95988d6e25f322048dfa34de409e5d6d81eb0847
growlife <james.piao128@gmail.com>
2021-05-24 12:59:36 +0000

    Update the profile card change, phone number

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -12,9 +12,15 @@ var Qr = require('../models/qrcode');
 
 var User = require('../models/users');
 
+var formidable = require('formidable');
+
+var path = require('path');
+
 var QRCode = require('qrcode');
 
 var router = express.Router();
+
+var fs = require('fs');
 
 var auth = function auth(req, res, next) {
   if (req.isAuthenticated()) return next();
@@ -26,11 +32,57 @@ router.get('/user/edit', auth, function (req, res, next) {
   res.locals.page_name = "Edit";
   res.render('user-edit');
 });
-router.post('/user/update-info', auth, /*#__PURE__*/function () {
+router.post('/user/uploadimage', auth, /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(req, res, next) {
+    var form, p;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
+          case 0:
+            form = new formidable.IncomingForm();
+            form.parse(req);
+            form.multiples = true; // store all uploads in the /uploads directory
+
+            form.uploadDir = path.join(__dirname, '../public/uploads');
+            p = new Promise(function (resolve, reject) {
+              form.on('file', function (field, file) {
+                var fileName = "";
+                fs.rename(file.path, path.join(form.uploadDir, file.name), function (err) {
+                  if (err) throw err;
+                  var file_path = '/uploads/' + file.name;
+                  resolve(file_path);
+                });
+              });
+            });
+            p.then(function (fileName) {
+              User.updateOne({
+                _id: req.user.id
+              }, {
+                $set: {
+                  profilePicture: fileName
+                }
+              }).then(function (data) {
+                return res.json(fileName);
+              });
+            });
+
+          case 6:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function (_x, _x2, _x3) {
+    return _ref.apply(this, arguments);
+  };
+}());
+router.post('/user/update-info', auth, /*#__PURE__*/function () {
+  var _ref2 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee2(req, res, next) {
+    return _regenerator["default"].wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
           case 0:
             User.updateOne({
               _id: req.user.id
@@ -43,14 +95,14 @@ router.post('/user/update-info', auth, /*#__PURE__*/function () {
 
           case 1:
           case "end":
-            return _context.stop();
+            return _context2.stop();
         }
       }
-    }, _callee);
+    }, _callee2);
   }));
 
-  return function (_x, _x2, _x3) {
-    return _ref.apply(this, arguments);
+  return function (_x4, _x5, _x6) {
+    return _ref2.apply(this, arguments);
   };
 }());
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const Qr = require('../models/qrcode');
 const User = require('../models/users'); 
+const formidable = require('formidable');
+const path = require('path');
 var QRCode = require('qrcode')
 const router = express.Router();
+const fs = require('fs');
 
 const auth = (req, res, next) => {
     if (req.isAuthenticated())
@@ -14,6 +17,31 @@ const auth = (req, res, next) => {
 router.get('/user/edit', auth, (req, res, next) => {
   res.locals.page_name = "Edit";
   res.render('user-edit');
+})
+
+router.post('/user/uploadimage', auth, async (req, res, next) => {
+  var form = new formidable.IncomingForm();
+  form.parse(req);
+  form.multiples = true;
+   // store all uploads in the /uploads directory
+   form.uploadDir = path.join(__dirname, '../public/uploads')
+   let p = new Promise((resolve, reject) => {
+      form.on('file', function (field, file) {
+         let fileName = "";
+         fs.rename(file.path, path.join(form.uploadDir, file.name), function (err) {
+            if (err) throw err;
+            const file_path = '/uploads/' + file.name;
+            resolve(file_path);
+         });
+      });
+   });
+   
+   p.then((fileName) => {
+      User.updateOne({_id: req.user.id}, {$set: {profilePicture: fileName}}).then(data => {
+         return res.json(fileName)
+      })
+      
+   });
 })
 
 router.post('/user/update-info', auth, async(req, res, next) => {
